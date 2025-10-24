a5941cd87a423c26ff59d1f261ac079f516c5779
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-10-24 18:10:38 +0200

    Basic homepage skeleton

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,67 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='flex flex-col justify-between min-h-lvh'>
+      <div>
+        <JLBoxHeader></JLBoxHeader>
+        <Home></Home>
+      </div>
+      <JLBoxFooter></JLBoxFooter>
+    </div>
+    
   </StrictMode>,
 )
+
+
+
+function JLBoxHeader() {
+  return (
+    <header className="navbar bg-base-100 shadow-sm justify-between py-4">
+      <div className="flex max-w-2xs">
+        <a className="btn btn-ghost text-xl">JLBox</a>
+      </div>
+      <div className="flex gap-2">
+        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+        <button className='btn btn-ghost'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>
+        </button>
+      </div>
+      <div className="flex gap-2 max-w-2xs">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  )
+}
+function JLBoxFooter() {
+  return (
+    <footer className='flex flex-row justify-center border border-neutral/5 py-4 ' ><p className='text-neutral/60'>José Antonio López Pérez - 2025</p></footer>
+  )
+}
+
+
---
+++ web/src/pages/Home.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex flex-col justify-center align-middle items-center min-h-full mt-8'>
+        <section className='grid grid-cols-3 w-4xl gap-2 p-8 shadow-sm'>
+            <WarehouseCard></WarehouseCard>
+            <WarehouseCard></WarehouseCard>
+            <WarehouseCard></WarehouseCard>
+            <WarehouseCard></WarehouseCard>
+            <WarehouseCard></WarehouseCard>
+            <WarehouseCard></WarehouseCard>
+        </section>
+
+        
+        <section className='flex flex-col w-4xl mt-8 pt-4 p-8 shadow-sm'>
+            <ItemsGridHeader></ItemsGridHeader>
+            <article className='grid grid-cols-4 gap-2'>
+                <ItemCard></ItemCard>
+                <ItemCard></ItemCard>
+                <ItemCard></ItemCard>
+                <ItemCard></ItemCard>
+                <ItemCard></ItemCard>
+                <ItemCard></ItemCard>
+                <ItemCard></ItemCard>
+            </article>
+            
+        </section>
+        
+    </div>
+    
+  )
+}
+
+function WarehouseCard() {
+  return (
+    <div className='bg-neutral-200 min-h-20'>
+        <div className='bg-neutral-300 h-8 flex flex-col w-full justify-center a'>
+            <p className='text-center'>Warehouse</p>
+        </div>
+
+    </div>
+  )
+}
+function ItemsGridHeader() {
+  return (
+    <div className='flex flex-row justify-end w-full mb-4'>
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn px-15">
+            Filtros
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 p-2 shadow">
+            <li ><p>Recientes primero</p></li>
+            <li><p>Antiguos primero</p></li>
+          </ul>
+        </div>
+
+    </div>
+  )
+}
+
+function ItemCard() {
+  return (
+    <div className='h-60'>
+        <div className='bg-neutral-200 h-45 flex flex-col w-full justify-center a'>
+        </div>
+        <div className='bg-neutral-300 h-15 flex flex-col w-full justify-center a'>
+            <p className='text-center'>Warehouse</p>
+        </div>
+    </div>
+  )
+}
+
+
+export default Home
