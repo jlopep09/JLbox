295d44dbf499a37653cc8c7e4d35cb457ebaf317
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-10-25 19:02:46 +0200

    Implemented get and post warehouses endpoints

--- api/routers/router_warehouse.py
+++ api/routers/router_warehouse.py
@@ -1,8 +1,39 @@
+from typing import Optional
 from fastapi import APIRouter
-
+from api.db.db_conector import conn,cur
+from fastapi import HTTPException, status
+from pydantic import BaseModel
 router = APIRouter(prefix="/wh", tags=["Warehouses"])
 
 
-@router.get("/all")
-async def get_all_wh():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+
+@router.get("/{owner_id}")
+async def get_all_wh_from_user_id(owner_id: int):
+    cur = conn.cursor()
+    cur.execute("SELECT id, name, description FROM warehouses WHERE owner_id = %s LIMIT 6",(owner_id,))
+    resultados = cur.fetchall()
+    return {"warehouses": resultados}
+
+class WarehouseCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+    owner_id: int
+
+@router.post("/")
+async def create_wh_by_user_id(wh :WarehouseCreate):
+    try:
+        cur.execute("SELECT COUNT(*) FROM warehouses WHERE owner_id = %s", (wh.owner_id,))
+        wh_count = int(cur.fetchone()[0])
+        if wh_count >= 6:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You cant create more than 6 warehouses.")
+        cur.execute("INSERT INTO warehouses (name, description, owner_id) VALUES (%s, %s, %s)",(wh.name, wh.description, wh.owner_id))
+        conn.commit()
+        return HTTPException(status_code=status.HTTP_201_CREATED)
+    except HTTPException:
+        raise
+    except Exception as e:
+        print(f"Error inseperado: {str(e)}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    finally:
+        cur.close()
+    
