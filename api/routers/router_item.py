43a88a5bd305c5fcb880582939a4caa37161fb3b
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-10-26 19:30:01 +0100

    implemented  multi item get endpoint

--- api/routers/router_item.py
+++ api/routers/router_item.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException,status
+from api.db.db_conector import get_connection
+router = APIRouter(prefix="/items", tags=["Items"])
 
-router = APIRouter(prefix="/item", tags=["Items"])
 
-
-@router.get("/all")
-async def get_all_items():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+@router.get("/{user_id}")
+async def get_items(user_id: int, offset:int = 0, limit:int = 30, conn = Depends(get_connection)):
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT id, name, description, count, box_id FROM items WHERE owner_id = %s LIMIT %s OFFSET %s", (user_id, limit, offset))
+        result = cur.fetchall()
+        return {"items": result}
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No se ha podido cargar los objetos solicitados.")
+    finally:
+        cur.close()
